4ecb00ff4da3524633a1ed38ca5975bb873e641e
Florian Chanson <f.chanson@barbablanka.com>
2023-01-19 22:38:44 +0100

    Upated page title

--- app/leaderboards/head.jsx
+++ app/leaderboards/head.jsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>HitW - Players</title>
+      <title>HitW - Leaderboards</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Check out the Hole in the Wall leaderboards!" />
       <link rel="icon" href="/images/favicon.png" />
