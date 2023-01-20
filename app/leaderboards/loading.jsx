56d52723c1df833a635b9bda3b7f40447151d4d4
Florian Chanson <f.chanson@barbablanka.com>
2023-01-20 13:41:29 +0100

    Improved skeleton on mobile devices

--- app/leaderboards/loading.jsx
+++ app/leaderboards/loading.jsx
@@ -12,12 +12,12 @@ export default function Loading() {
             {[...Array(10).keys()].map((index) => (
                 <div className={`w-full p-4 shadow-lg ${index == 0 ? "lg:rounded-t-lg border-t lg:border-x" : index == 9 ? "lg:rounded-b-lg border-b lg:border-x" : 'lg:border-x' } ${index % 2 == 0 ? "bg-[#404040] border-[#606060]" : "bg-[#303030] border-[#505050]"}`}>
                     <div className="flex w-full items-center justify-between">
-                        <div className="flex items-center lg:space-x-10">
-                        <div className="bg-gray-500 w-16 h-10 rounded-xl animate-pulse"/>
-                            <div className="w-10 h-10 bg-gray-500 animate-pulse rounded-xl" />
-                            <div className="bg-gray-500 w-48 h-10 rounded-xl animate-pulse"/>
+                        <div className="flex items-center space-x-2 lg:space-x-10">
+                        <div className="bg-gray-500 w-12 h-8 lg:w-16 lg:h-10 rounded-xl animate-pulse"/>
+                            <div className="w-8 h-8 lg:w-10 lg:h-10 bg-gray-500 animate-pulse rounded-xl" />
+                            <div className="bg-gray-500 w-32 lg:w-48 h-8 lg:h-10 rounded-xl animate-pulse"/>
                         </div>
-                        <div className="bg-gray-500 w-24 h-10 rounded-xl animate-pulse"/>
+                        <div className="bg-gray-500 w-16 lg:w-24 h-8 lg:h-10 rounded-xl animate-pulse"/>
                     </div>
                 </div>
             ))}
