7c6fcb6bf335f76b426d769cdea0e66d8da68497
Florian Chanson <f.chanson@barbablanka.com>
2023-01-20 13:54:21 +0100

    test

--- components/PlayerListLoader.jsx
+++ components/PlayerListLoader.jsx
@@ -1,7 +1,7 @@
 import PlayerList from "./PlayerList"
 
-const PlayerListLoader = () => {
-    const users = getUsers()
+const PlayerListLoader = async () => {
+    const users = await getUsers()
 
     return (
         <PlayerList users={users} />
@@ -10,12 +10,12 @@ const PlayerListLoader = () => {
 
 export default PlayerListLoader
 
-function getUsers() {
+async function getUsers() {
     let users
     
     try {
-        const data = fetch(`${process.env.HOST}users?token=${process.env.TOKEN}`)
-        users = data.json()
+        const data = await fetch(`${process.env.HOST}users?token=${process.env.TOKEN}`)
+        users = await data.json()
     } catch {}
   
     return users?.users
